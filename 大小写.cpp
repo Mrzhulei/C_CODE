a85b4ce2276ccb900c9173cb03e907b34795e528
Mrzhulei <110291706+Mrzhulei@users.noreply.github.com>
2022-09-13 19:05:20 +0800

    Add files via upload

---
+++ 大小写.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  char ch;
+  cin>>ch;
+  ch=ch-32;
+  cout<<ch<<endl;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+	
+	
+ } 
---
+++ 求圆柱面积和体积.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a=3,b=5,c;
+	c=0;
+	c=b;
+	b=a;
+	a=c;
+	cout<<a<<endl;
+	cout<<b<<endl;
+	
+	
+	return 0;
+	
+	
+	
+ } 
